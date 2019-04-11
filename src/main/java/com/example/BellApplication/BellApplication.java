292b3ca8a03e458a43f0ffff4bfd7fbea8580d0a
a1 <jambau93@gmail.com>
2019-04-11 21:53:22 +0300

    Add basic config.

---
+++ src/main/java/com/example/BellApplication/BellApplication.java
@@ -0,0 +1,13 @@
+package com.example.BellApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellApplication.class, args);
+	}
+
+}
